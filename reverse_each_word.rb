34dce651cd35c4e123a9c5ca59bdd6998f3f07ab
krishnendu basak <krisauz17@yahoo.com>
2022-05-23 19:32:12 +1000

    REverse Each Word lab is done

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+def reverse_each_word (sentence1)#("Hello there, are how are you?")
+    sentence1 = "Hello there, are how are you?".split.reverse.join(" ").reverse!
+    puts "Hello there, are how are you?".split
+    puts "Hello there, are how are you?".split.reverse
+    puts "Hello there, are how are you?".split.reverse.join (" ")
+    puts "Hello there, are how are you?".split.reverse.join(" ").reverse
+end
+puts "Hello there, are how are you?".split.reverse.join(" ").reverse!
+
+def reverse_each_word (sentence2)#("Hi again, just making sure it's reversed!")
+    sentence2 = "Hi again, just making sure it's reversed!".split.reverse.join(" ").reverse!
+    puts "Hi again, just making sure it's reversed!".split
+    puts "Hi again, just making sure it's reversed!".split.reverse
+    puts "Hi again, just making sure it's reversed!".split.reverse.join(" ")
+    puts "Hi again, just making sure it's reversed!".split.reverse.join(" ").reverse
+end
+puts "Hi again, just making sure it's reversed!".split.reverse.join(" ").reverse!
+
